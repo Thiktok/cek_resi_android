926e7d19b0e722b32ab26c8ed5c556f55aed1008
Ramadhan Maulana <ramadhancm@gmail.com>
2020-09-13 18:32:12 +0700

    add layout for custom view

---
+++ app/src/main/java/com/ramadhan/couriertracking/customview/InfoTextView.kt
@@ -0,0 +1,36 @@
+package com.ramadhan.couriertracking.customview
+
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.view.View
+import android.widget.LinearLayout
+import com.ramadhan.couriertracking.R
+import kotlinx.android.synthetic.main.info_text_view.view.*
+
+class InfoTextView(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+
+    init {
+        View.inflate(context, R.layout.info_text_view, this)
+
+        val attributes  = context.obtainStyledAttributes(attrs, R.styleable.InfoTextView)
+
+        customInfoValue.setText("hello")
+        customInfoLabel.text = attributes.getString(R.styleable.InfoTextView_textLabel)
+        customInfoValue.text = attributes.getString(R.styleable.InfoTextView_textValue)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            customInfoLabel.setTextColor(attributes.getColor(R.styleable.InfoTextView_labelColor,
+                context.getColor(R.color.defaultTextColor)))
+            customInfoValue.setTextColor(attributes.getColor(R.styleable.InfoTextView_valueColor,
+                context.getColor(R.color.defaultTextColor)))
+        }else{
+            customInfoLabel.setTextColor(attributes.getColor(R.styleable.InfoTextView_labelColor,
+                resources.getColor(R.color.defaultTextColor)))
+            customInfoValue.setTextColor(attributes.getColor(R.styleable.InfoTextView_valueColor,
+                resources.getColor(R.color.defaultTextColor)))
+        }
+
+        attributes.recycle()
+    }
+}
