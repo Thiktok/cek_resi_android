ca382e94f19668cff2ca42e65bc2722e6cb93c2d
Ramadhan Maulana <ramadhancm@gmail.com>
2020-09-13 15:13:12 +0700

    add list to spinner

--- app/src/main/java/com/ramadhan/couriertracking/view/MainActivity.kt
+++ app/src/main/java/com/ramadhan/couriertracking/view/MainActivity.kt
@@ -2,24 +2,35 @@ package com.ramadhan.couriertracking.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.Spinner
+import android.widget.Toast
 import com.google.gson.Gson
 import com.ramadhan.couriertracking.R
 import com.ramadhan.couriertracking.data.response.entity.Courier
 import com.ramadhan.couriertracking.view.adapter.CourierSpinnerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+    private lateinit var courierAdapter: CourierSpinnerAdapter
+    private lateinit var courierList: List<Courier>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mainCourierList.adapter = CourierSpinnerAdapter(
-            this,
-            readFromAsset()
-        )
+        courierList = readFromAsset()
+
+        courierAdapter = CourierSpinnerAdapter(this, courierList)
+        mainCourierList.adapter = courierAdapter
+
+        val spinner: Spinner = findViewById(R.id.mainCourierList)
+        spinner.onItemSelectedListener = this
     }
 
-    private fun readFromAsset(): List<Courier>{
+    private fun readFromAsset(): List<Courier> {
         val fileName = "courier_list.json"
 
         val bufferReader = application.assets.open(fileName).bufferedReader()
@@ -28,8 +39,20 @@ class MainActivity : AppCompatActivity() {
             it.readText()
         }
         val gson = Gson()
-        val courierList = gson.fromJson(jsonString, Array<Courier>::class.java).toList()
 
-        return courierList
+        return gson.fromJson(jsonString, Array<Courier>::class.java).toList()
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val selectedItem = parent?.getItemAtPosition(position) as Courier
+        Toast.makeText(
+            this,
+            "Selected item is ${selectedItem.name}",
+            Toast.LENGTH_SHORT
+        ).show()
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+
     }
 }
