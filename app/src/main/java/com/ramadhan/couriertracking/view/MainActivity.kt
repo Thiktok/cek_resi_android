35b34b6bb0709b2bef43fce76236ee1501ab20ff
Ramadhan Maulana <ramadhancm@gmail.com>
2020-09-13 10:37:01 +0700

    update main layout

--- app/src/main/java/com/ramadhan/couriertracking/view/MainActivity.kt
+++ app/src/main/java/com/ramadhan/couriertracking/view/MainActivity.kt
@@ -2,11 +2,34 @@ package com.ramadhan.couriertracking.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
 import com.ramadhan.couriertracking.R
+import com.ramadhan.couriertracking.data.response.entity.Courier
+import com.ramadhan.couriertracking.view.adapter.CourierSpinnerAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainCourierList.adapter = CourierSpinnerAdapter(
+            this,
+            readFromAsset()
+        )
+    }
+
+    private fun readFromAsset(): List<Courier>{
+        val fileName = "courier_list.json"
+
+        val bufferReader = application.assets.open(fileName).bufferedReader()
+
+        val jsonString = bufferReader.use {
+            it.readText()
+        }
+        val gson = Gson()
+        val courierList = gson.fromJson(jsonString, Array<Courier>::class.java).toList()
+
+        return courierList
     }
 }
