2adb4101682e875cbc776ea029cdfdc42bc0eda5
usworder@gmail.com <G0g0pr4y>
2020-09-19 10:14:27 +0700

    create recyclerview adapter

---
+++ app/src/main/java/com/ramadhan/couriertracking/view/adapter/TrackingRecyclerViewAdapter.kt
@@ -0,0 +1,56 @@
+package com.ramadhan.couriertracking.view.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ramadhan.couriertracking.R
+import com.ramadhan.couriertracking.data.response.entity.Tracking
+import kotlinx.android.synthetic.main.item_tracking_info.view.*
+
+class TrackingRecyclerViewAdapter(
+    private val context: Context,
+    private var trackingList: MutableList<Tracking>
+) : RecyclerView.Adapter<TrackingRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): TrackingRecyclerViewAdapter.ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.item_tracking_info, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: TrackingRecyclerViewAdapter.ViewHolder, position: Int) {
+        val item = trackingList[position]
+        holder.bindItem(item)
+    }
+
+    override fun getItemCount(): Int {
+        return trackingList.size
+    }
+
+    fun addItem(tracking: Tracking){
+        trackingList.add(tracking)
+    }
+
+    fun addItems(tracking: List<Tracking>){
+        trackingList.addAll(tracking)
+    }
+
+    fun updateItem(tracking: List<Tracking>){
+        trackingList.clear()
+        trackingList.addAll(tracking)
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        fun bindItem(tracking: Tracking) {
+            itemView.itemInfoDate.text = tracking.date.subSequence(0, 10)
+            itemView.itemInfoTime.text = tracking.date.drop(10)
+            itemView.itemInfoDetail.text = tracking.desc
+        }
+    }
+}
